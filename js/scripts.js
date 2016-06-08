5163d6e9bc25af0fe313fe2bb391c0d62eb0f035
Adam Luchini and Kyle Helyar <kyle@>
2016-06-08 11:57:42 -0700

    add conditional to not allow negative number

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 var leapYear = function(inputYear) {
-  if ((inputYear % 4 === 0) && (inputYear % 100 !== 0) || (inputYear % 400 === 0)){
-  }
+  return ((inputYear % 4 === 0) && (inputYear % 100 !== 0) || (inputYear % 400 === 0))
+
 };
 
 $(function () {
   $('form#input').submit(function(event) {
     event.preventDefault();
     var inputYear = $('input#inputYear').val();
-    var result = leapYear(inputYear);
 
-    if (result === true) {
+
+    // if (inputYear < 0) {
+    //   $('.negative').show();
+    // }
+
+    if (leapYear(inputYear)) {
       $('.not').text('');
     } else {
-        $('.not').text('not');
-    }
-    $('.yearText').text(inputYear);
+        $('.not').text('not ');
+      }
+
     $('#result').show();
+    $('.inputYear').text(inputYear);
+
+    if (inputYear < 0) {
+      $('.negative').text('Please enter a positive number.');
+    }
   });
 });
